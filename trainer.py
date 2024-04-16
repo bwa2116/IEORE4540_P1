f8db4fb5e68d8dc65bfd3c65058bbdb217c0fcdd
bwa2116 <151790222+bwa2116@users.noreply.github.com>
2024-04-17 01:08:26 +0200

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -13,10 +13,9 @@ class Trainer:
         self.config = config
         self.optimizer = optimizer
         self.loss_fn = loss_fn
-        self.exp_name = exp_name
         self.device = device
 
-    def train(self, exp_name,
+    def train(self,
               trainloader, testloader,
               epochs, save_model_every_n_epochs=0):
         """
